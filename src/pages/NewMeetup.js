4054508203005ad09bb077eef61e4effc6d63733
Katlego <moimakatlego2@gmail.com>
2021-12-20 14:46:45 +0200

    adding new meetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage () {
+
+    const navigate = useNavigate();
+
     function addMeetupHandler(meetupData) {
+
         fetch('https://react-refresher-93981-default-rtdb.firebaseio.com/meetups.json',
 
             {
@@ -12,7 +18,9 @@ function NewMeetupPage () {
                 }
             }
         
-        );
+        ).then(() => {
+            navigate('/');
+        });
     }
     
 
